74f731c126182bfb2a4c67e71220d8a55d37854c
SlackE123 <Slack.e@etoncollege.org.uk>
2024-01-19 10:13:57 +0000

    Initial Commit

---
+++ CarCustomiser/CarCustomiserApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarCustomiserApp.swift
+//  CarCustomiser
+//
+//  Created by Ed Slack on 19/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct CarCustomiserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
